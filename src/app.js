924003a33e5b21cb02cd21105f422dcdf31a5671
ParraJuanPablo <parrfjp@gmail.com>
2023-03-24 18:37:59 +0000

    Mi primer commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  generateDomains();
 };
+
+function generateDomains() {
+  var pronombre = ["el", "nuestro"];
+  var adjetivo = ["gran", "buen", "sorprendente"];
+  var nombre = ["lugar", "gato", "perro"];
+  var terminal = [".com", ".es", ".net"];
+  for (let i = 0; i < pronombre.length; i++) {
+    for (let j = 0; j < adjetivo.length; j++) {
+      for (let k = 0; k < nombre.length; k++) {
+        for (let l = 0; l < terminal.length; l++) {
+          console.log(pronombre[i] + adjetivo[j] + nombre[k] + terminal[l]);
+        }
+      }
+    }
+  }
+}
